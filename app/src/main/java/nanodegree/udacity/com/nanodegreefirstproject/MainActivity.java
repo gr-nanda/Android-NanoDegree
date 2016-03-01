bd3418e8f0be9366912f3bf7aef537b4cca92062
gr-nanda <nandakumar_gce1@yahoo.co.in>
2016-03-01 17:53:02 +0530

    first project - layout

---
+++ app/src/main/java/nanodegree/udacity/com/nanodegreefirstproject/MainActivity.java
@@ -0,0 +1,51 @@
+package nanodegree.udacity.com.nanodegreefirstproject;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+
+    public void displayMessage(View view)
+    {
+        Button b = (Button)view;
+        String buttonText = b.getText().toString();
+        int duration = Toast.LENGTH_SHORT;
+        CharSequence text = "This button will launch my " + buttonText + " app!";
+        Toast.makeText(this, text, duration).show();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
